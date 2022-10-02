0028368fc92ecc74249550558085be53cb91a1a1
kongfl888 K <kongfl888@outlook.com>
2022-10-02 21:37:37 +0800

    direct path and do more things for adhook. only for backup.

--- luci-app-adbyby-plus/root/usr/share/adbyby/adbybyfirst.sh
+++ luci-app-adbyby-plus/root/usr/share/adbyby/adbybyfirst.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-PROG_PATH=$(pwd)
+PROG_PATH=$(cd $(dirname $0); pwd)
+
+[ $(awk -F= '/^ipset/{print $2}' $PROG_PATH/adhook.ini) -eq 1 ] && \
+{
+	sed -i 's/adbyby_list/adbyby_wan/' /tmp/adbyby_host.conf
+	echo conf-file=/tmp/adbyby_host.conf >> /tmp/dnsmasq.d/dnsmasq-adbyby.conf
+	/etc/init.d/dnsmasq restart
+}
+
 $PROG_PATH/adbybyupdate.sh
